742cbad03c0d92b5c38afd2fb5f2425c34684e51
sophxmia <maliarenko.sofiia@gmail.com>
2024-02-09 13:04:52 +0200

    Implement methods for reading and writing data about souvenirs and manufacturers to files.

--- src/main/java/souvenirs/control/FileHandler.java
+++ src/main/java/souvenirs/control/FileHandler.java
@@ -1,5 +1,8 @@
 package souvenirs.control;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,20 +11,75 @@ public class FileHandler {
     public List<Souvenir> readSouvenirsFromFile(String fileName) {
         List<Souvenir> souvenirs = new ArrayList<>();
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split((","));
+                String name = data[0];
+                String producerName = data[1];
+                String country = data[2];
+                LocalDate releaseDate = LocalDate.parse(data[3], formatter);
+                double price = Double.parseDouble(data[4]);
+
+                Producer producer = new Producer(producerName, country);
+                Souvenir souvenir = new Souvenir(name, producer, releaseDate, price);
+                souvenirs.add(souvenir);
+            }
+        } catch (IOException e) {
+//            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
         return souvenirs;
     }
 
-    public void writeSouvenirsToFile(List<Souvenir> souvenirs, String fileName){
+    public void writeSouvenirsToFile(List<Souvenir> souvenirs, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+            for (Souvenir souvenir : souvenirs) {
+                String releaseDateFormatted = souvenir.getReleaseDate().format(formatter);
+                writer.write(souvenir.getName() + "," + souvenir.getProducer().getName() + "," +
+                        souvenir.getProducer().getCountry() + "," + releaseDateFormatted + "," +
+                        souvenir.getPrice() + "\n");
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Producer> readProducersFromFile(String fileName) {
         List<Producer> producers = new ArrayList<>();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                String name = data[0];
+                String country = data[1];
+
+                Producer producer = new Producer(name, country);
+                producers.add(producer);
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         return producers;
     }
 
-    public void writeProducersToFile(List<Producer> producers, String fileName){
-
+    public void writeProducersToFile(List<Producer> producers, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (Producer producer : producers) {
+                writer.write(producer.getName() + ","
+                        + producer.getCountry() + "\n");
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
