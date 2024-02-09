80772229bbc14dc68e390840258c64d15eb901a0
sophxmia <maliarenko.sofiia@gmail.com>
2024-02-09 11:04:30 +0200

    Initial commit

---
+++ src/main/java/souvenirs/control/Main.java
@@ -0,0 +1,7 @@
+package souvenirs.control;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/souvenirs/control/Souvenir.java
@@ -0,0 +1,2 @@
+package souvenirs.control;public class Souvenir {
+}
