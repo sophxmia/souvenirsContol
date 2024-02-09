a0f698c494a226e15fbbebccb5e554f875c2d0e1
sophxmia <maliarenko.sofiia@gmail.com>
2024-02-09 11:46:58 +0200

    Add class FileHandler with methods for reading and writing data about souvenirs and manufacturers to files.

---
+++ src/main/java/souvenirs/control/FileHandler.java
@@ -0,0 +1,27 @@
+package souvenirs.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+
+    public List<Souvenir> readSouvenirsFromFile(String fileName) {
+        List<Souvenir> souvenirs = new ArrayList<>();
+
+        return souvenirs;
+    }
+
+    public void writeSouvenirsToFile(List<Souvenir> souvenirs, String fileName){
+
+    }
+
+    public List<Producer> readProducersFromFile(String fileName) {
+        List<Producer> producers = new ArrayList<>();
+
+        return producers;
+    }
+
+    public void writeProducersToFile(List<Producer> producers, String fileName){
+
+    }
+}
