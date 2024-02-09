74b06a99e2a9889fcf967f66c0c4f35e0b616bb1
sophxmia <maliarenko.sofiia@gmail.com>
2024-02-09 11:13:37 +0200

    Add the Souvenir and Producer methods

--- src/main/java/souvenirs/control/Producer.java
+++ src/main/java/souvenirs/control/Producer.java
@@ -1,2 +1,43 @@
-package souvenirs.control;public class Producer {
+package souvenirs.control;
+
+public class Producer {
+    private String name;
+    private String country;
+
+    /**
+     * Class constructor
+     * @param name producer's name
+     * @param country country
+     */
+    public Producer (String name, String country){
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    /**
+     * Override the toString() method to have a better view of the object Producer
+     * @return Producer
+     */
+    @Override
+    public String toString() {
+        return "Producer{" +
+                "name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
--- src/main/java/souvenirs/control/Souvenir.java
+++ src/main/java/souvenirs/control/Souvenir.java
@@ -1,2 +1,70 @@
-package souvenirs.control;public class Souvenir {
+package souvenirs.control;
+
+import java.time.LocalDate;
+
+public class Souvenir {
+    private String name;
+    private Producer producer;
+    private LocalDate releaseDate;
+    private Double price;
+
+    /**
+     * Class constructor
+     * @param name name of the product
+     * @param producer producer
+     * @param releaseDate releaseDate
+     * @param price price
+     */
+    public Souvenir(String name, Producer producer, LocalDate releaseDate, double price){
+        this.name = name;
+        this.producer = producer;
+        this.releaseDate = releaseDate;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    /**
+     * Override the toString() method to have a better view of the object Souvenir
+     * @return Souvenir
+     */
+    @Override
+    public String toString(){
+        return "Souvenir{" +
+                "name='" + name + '\'' +
+                ", producer=" + producer +
+                ", releaseDate=" + releaseDate +
+                ", price=" + price +
+                '}';
+    }
 }
