8e8578411144c9e67fab8970f5aed5875212b5b1
Sofiia Maliarenko <maliarenko.sofiia@gmail.com>
2024-02-21 11:44:26 +0200

    Update the Edit and Delete options

--- src/main/java/souvenirs/control/ViewDataWindow.java
+++ src/main/java/souvenirs/control/ViewDataWindow.java
@@ -5,9 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,18 +15,17 @@ import java.util.List;
 
 public class ViewDataWindow extends Application {
     private final DataManagerFacade dataManagerFacade;
+    private TableView<Souvenir> tableView;
 
     public ViewDataWindow(DataManagerFacade dataManagerFacade) {
         this.dataManagerFacade = dataManagerFacade;
     }
-    private TableView<Souvenir> tableView;
 
     @Override
     public void start(Stage primaryStage) {
-        tableView = new TableView<>();
         primaryStage.setTitle("View Data about Souvenir");
 
-        TableView<Souvenir> tableView = new TableView<>();
+        tableView = new TableView<>();
         ObservableList<Souvenir> souvenirs = FXCollections.observableArrayList(dataManagerFacade.getAllSouvenirs());
 
         TableColumn<Souvenir, String> nameColumn = new TableColumn<>("Name");
@@ -43,46 +40,74 @@ public class ViewDataWindow extends Application {
         TableColumn<Souvenir, Double> priceColumn = new TableColumn<>("Price");
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-        tableView.getColumns().addAll(nameColumn, producersColumn, releaseDateColumn, priceColumn);
+        TableColumn<Souvenir, Void> editButtonColumn = new TableColumn<>("Edit");
+        editButtonColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button editButton = new Button("Edit");
+
+            {
+                editButton.setOnAction(event -> {
+                    Souvenir souvenir = getTableView().getItems().get(getIndex());
+                    editSouvenir(souvenir);
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(editButton);
+                }
+            }
+        });
+
+        TableColumn<Souvenir, Void> deleteButtonColumn = new TableColumn<>("Delete");
+        deleteButtonColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button deleteButton = new Button("Delete");
+
+            {
+                deleteButton.setOnAction(event -> {
+                    Souvenir souvenir = getTableView().getItems().get(getIndex());
+                    deleteSouvenir(souvenir);
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(deleteButton);
+                }
+            }
+        });
+
+        tableView.getColumns().addAll(nameColumn, producersColumn, releaseDateColumn, priceColumn, editButtonColumn, deleteButtonColumn);
         tableView.setItems(souvenirs);
 
-        Button editSouvenirButton = new Button("Edit Souvenir");
-        editSouvenirButton.setOnAction(event -> editSouvenir());
-
-        Button deleteSouvenirButton = new Button("Delete Souvenir");
-        deleteSouvenirButton.setOnAction(event -> deleteSouvenir());
-
-        VBox vbox = new VBox(tableView, editSouvenirButton, deleteSouvenirButton);
+        VBox vbox = new VBox(tableView);
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(10);
 
-        Scene scene = new Scene(vbox, 600, 400);
+        Scene scene = new Scene(vbox, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void editSouvenir() {
-        Souvenir selectedSouvenir = getSelectedSouvenir();
-        if (selectedSouvenir != null) {
-            EditDataWindow editDataWindow = new EditDataWindow(dataManagerFacade);
-            editDataWindow.setSouvenir(selectedSouvenir);
-            editDataWindow.start(new Stage());
-        }
+    private void editSouvenir(Souvenir souvenir) {
+        EditDataWindow editDataWindow = new EditDataWindow(dataManagerFacade);
+        editDataWindow.setSouvenir(souvenir);
+        editDataWindow.start(new Stage());
     }
 
-    private void deleteSouvenir() {
-        Souvenir selectedSouvenir = getSelectedSouvenir();
-        if (selectedSouvenir != null) {
-            dataManagerFacade.deleteSouvenir(selectedSouvenir);
-        }
-    }
-
-    private Souvenir getSelectedSouvenir() {
-        return tableView.getSelectionModel().getSelectedItem();
+    private void deleteSouvenir(Souvenir souvenir) {
+        dataManagerFacade.deleteSouvenir(souvenir);
+        tableView.getItems().remove(souvenir);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
